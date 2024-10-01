5b7139b117b35c87210a9e53a2840cffc0ff55f4
Roi Verano <roiverano20@gmail.com>
2024-10-02 03:27:13 +0800

    DAY-70

--- ExpenseTracker/wwwroot/js/site.js
+++ ExpenseTracker/wwwroot/js/site.js
@@ -1,26 +1,16 @@
-﻿
-
-var sideBar = document.getElementById('sideBar');
+﻿var sideBar = document.getElementById('sideBar');
 var toggleButton = document.getElementById('toggle-btn');
 var mainLayout = document.getElementById('main-layout');
 
 
 
+
+
 function sidebarOnclick() {
+    const sidebarComputedStyle = window.getComputedStyle(sideBar);
 
+    if (sidebarComputedStyle.display === 'none') {
 
-    if (sideBar.style.display == 'block') {
-
-        sideBar.style.opacity = '0';
-        console.log('clicked hide');
-
-        setTimeout(() => {
-            sideBar.style.display = 'none';
-            mainLayout.style.marginLeft = '0';
-        }, 400);
-   
-    }
-    else {
 
         sideBar.style.display = 'block';
         mainLayout.style.marginLeft = '-220px'
@@ -30,12 +20,23 @@ function sidebarOnclick() {
             sideBar.style.opacity = '1';
         }, 100)
 
-        
     }
+    else {
 
+        sideBar.style.opacity = '0';
+        console.log('clicked hide');
 
-
+        setTimeout(() => {
+            sideBar.style.display = 'none';
+            mainLayout.style.marginLeft = '0';
+        }, 400);
+    }
+    
 };
+
+
+
+
 
 
 
