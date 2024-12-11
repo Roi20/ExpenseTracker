ffb1657916504188927808a8ad65616094fd9e7c
Roi Verano <roiverano20@gmail.com>
2024-12-12 05:51:16 +0800

    Final

---
+++ ExpenseTracker/wwwroot/js/logincredentials.js
@@ -0,0 +1,20 @@
+﻿var usernameInput = document.getElementById('username-input');
+var passwordInput = document.getElementById('password-input');
+var loginForm = document.getElementById('account');
+
+
+document.getElementById('admin-credential').addEventListener('click', function () {
+
+    usernameInput.value = "admin@admin.com";
+    passwordInput.value = "@Password123";
+    loginForm.submit();
+
+});
+
+
+document.getElementById('user-credential').addEventListener('click', function () {
+
+    usernameInput.value = "joenorgg25@gmail.com";
+    passwordInput.value = "@Password123";
+    loginForm.submit();
+});
